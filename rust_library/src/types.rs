88def295ea088192b1c01cd4d19b2f9f8a7d002e
Zack <zack@zacharymaril.com>
2021-11-27 22:39:51 -0500

    Reducing code down in this branch

--- rust_library/src/types.rs
+++ rust_library/src/types.rs
@@ -1,57 +1,13 @@
 use std::collections::HashSet;
 
 #[derive(Debug, Eq, PartialEq)]
-pub struct PuzzleO {
-    pub layout: Grid,
-    pub givens: Vec<Cell>,
-    pub rules: Vec<Rule>
-}
-
-pub type Puzzle = [[u8; 9]; 9];
+pub struct A {}
 
 #[derive(Debug, Eq, PartialEq)]
-pub struct Rule {
-
-}
+pub struct B {}
 
 #[derive(Debug, Eq, PartialEq)]
-pub struct Cell {
-     //coordinates: Coordinate,
-     //value: CellValue 
-}
-
-// pub struct Pair {
-//     values: (u8, u8)
-// }
-
-// pub struct Triple {
-//     values: (u8, u8, u8)
-// }
-
-// pub struct Candidates {
-//     values: HashSet<u8>
-// }
-
-// pub enum CellValue {
-//     u8, 
-//     Pair,
-//     Triple,
-//     Candidates 
-// }
-
-// pub struct Coordinate {
-//     x: u8, 
-//     y: u8
-// }
+pub struct C {}
 
 #[derive(Debug, Eq, PartialEq)]
-pub struct Grid {
-
-}
-
-// pub struct Pattern {
-
-// }
-
-// pub struct Solution  {
-// }
+pub struct Grid {}
