a972079194aadb5ced9cbfb38309d494d1ad6590
Zack <zack@zacharymaril.com>
2021-11-27 22:41:20 -0500

    Removing more code

--- rust_library/tests/test.rs
+++ rust_library/tests/test.rs
@@ -1,33 +1,6 @@
 use penmark::*; 
 
 #[test]
-fn basic_sudoku() {
-    let rules = vec![Rule{}];
-    let givens = vec![Cell{}];
-    let layout = Grid{};
-    let sudoku = Puzzle{rules, givens, layout}; 
-
-    
-    let result = 2 + 2;
-    assert_eq!(result, 4);
-}
-
-#[test]
 fn wrong_sudoku(){
-    let wrong = "
-    1 2 3 | . . . | . . . 
-    1 2 3 | . . . | . . . 
-    1 2 3 | . . . | . . . 
-    - - - + - - - + - - - 
-    1 2 3 | . . . | . . . 
-    1 2 3 | . . . | . . . 
-    1 2 3 | . . . | . . . 
-    - - - + - - - + - - - 
-    1 2 3 | . . . | . . . 
-    1 2 3 | . . . | . . . 
-    1 2 3 | . . . | . . . 
-    ";
-
-    let results = parse_sudoku(wrong);
-    assert_eq!(results.layout, Grid{});
+    assert_eq!(1, 2);
 }
